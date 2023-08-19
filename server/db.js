d6e62ce373a0a4bbfbbc3947491660fe5ab433cd
Fabian <95060643+Achifa@users.noreply.github.com>
2023-08-19 20:46:25 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,18 +1,18 @@
 const {Client,Pool} = require('pg');
 
-let host = ["localhost", "postgresql-138493-0.cloudclusters.net", "postgres://fabian:woMRHytKIcBdf8l70B4h6BVv0qy26EIN@dpg-cjgh4ek1ja0c73cso17g-a.oregon-postgres.render.com/lodger"];
+let host = ["localhost", "postgresql-140872-0.cloudclusters.net"];
 let port = [5432,17935];
-let user = ["postgres", "fabian"];
-let password = ["asdfghjkl", "woMRHytKIcBdf8l70B4h6BVv0qy26EIN"];
-let db = ["lodger"];
+let user = ["postgres", "Fabian"];
+let password = ["asdfghjkl"];
+let db = ["Lodger"];
 
 let connectToDatabase = new Promise((resolve, reject) => {
 
     let pool = new Pool({
-        host: host[2],
-        port: port[0],
+        host: host[1],
+        port: port[1],
         user: user[1],
-        password: password[1],
+        password: password[0],
         database: db[0]
     });
  
